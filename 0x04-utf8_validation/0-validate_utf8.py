09bc9b3f3e185ae2978e10b05ed5c739a05c1008
Nkemjika1divine <cndivine@gmail.com>
2024-05-09 02:08:15 +0000

    UTF8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for validUTF8
+"""
+
+
+def validUTF8(data):
+    """VALIDUTF8 method"""
+    expected_continuations = 0
+    for byte in data:
+        if expected_continuations == 0:
+            if (byte >> 5) == 0b110:
+                expected_continuations = 1
+            elif (byte >> 4) == 0b1110:
+                expected_continuations = 2
+            elif (byte >> 3) == 0b11110:
+                expected_continuations = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            expected_continuations -= 1
+    return expected_continuations == 0
